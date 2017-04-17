ea0d94460b046c69cfb0ae3b1b4589f03583af82
gypsyCoder <shovan@outlook.com>
2017-04-18 03:46:22 +0600

    Update Libraries.

--- Dhobi/Dhobi.Admin.Web/Controllers/HomeController.cs
+++ Dhobi/Dhobi.Admin.Web/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Dhobi.Admin.Web.Controllers
 {
@@ -10,21 +6,8 @@ namespace Dhobi.Admin.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            ViewBag.Title = "Dhobi | Home";
+            return View("Login");
         }
     }
 }
