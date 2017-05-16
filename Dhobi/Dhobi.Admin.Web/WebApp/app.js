8b904f5fcf43ffe5923782edbfbddd6f7ce491ba
gypsyCoder <shovan@outlook.com>
2017-05-17 03:25:58 +0600

    State Change Wait Loader add.

--- Dhobi/Dhobi.Admin.Web/WebApp/app.js
+++ Dhobi/Dhobi.Admin.Web/WebApp/app.js
@@ -46,8 +46,13 @@
     });
 
     app.run(function ($rootScope, $location, $cookieStore, $localStorage, modalFactory, toastr, $state) {
-        $rootScope.$on('$stateChangeSuccess', function (event, toState) {
+        $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+            $("#ui-view").html("");
+            $(".page-loading").removeClass("hidden");
+        });
 
+        $rootScope.$on('$stateChangeSuccess', function (event, toState) {
+            $(".page-loading").addClass("hidden");
             $rootScope.CURRENTSTATE = toState.name;
             var token = $cookieStore.get('accessToken');
             if (!token || token.length < 1) {
@@ -63,6 +68,7 @@
                     $location.path('/login');
                 }
             }
+            
         });
 
         // Modal Popup and If window resize:Modal won't break
