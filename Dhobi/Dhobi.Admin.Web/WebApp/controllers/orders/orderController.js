a4ae119f5cc786e2819cec47a80c554ad91760b8
gypsyCoder <shovan@outlook.com>
2017-07-21 15:45:57 +0600

    Order Filter with date complete.

--- Dhobi/Dhobi.Admin.Web/WebApp/controllers/orders/orderController.js
+++ Dhobi/Dhobi.Admin.Web/WebApp/controllers/orders/orderController.js
@@ -1,6 +1,6 @@
-﻿define(['app', 'underscore', 'dir-pagination'], function (app, _) {
-    app.controller('orderController', ['$scope', 'apiConstant', 'httpService', '$timeout',
-        function ($scope, apiConstant, httpService, $timeout) {
+﻿define(['app', 'underscore', 'dir-pagination', 'i-check'], function (app, _) {
+    app.controller('orderController', ['$scope', 'apiConstant', 'httpService', '$timeout', 'moment',
+        function ($scope, apiConstant, httpService, $timeout, moment) {
             "use strict";
 
             $scope.currentPage = 1;
@@ -10,27 +10,38 @@
                 Orders: [],
                 TotalOrders: 0,
                 ShowingFrom: 0,
-                ShowingTo: 0
+                ShowingTo: 0,
+                SelectedOrders: [],
+                IsSelectedOrder: false,
+
+                SelectedDate: moment(),
+                MinStartDate: moment(),
+                MinEndDate: moment(),
             };
 
             $scope.Methods = {
                 Init: function () {
-                    //$scope.Methods.GetAllOrders($scope.currentPage);
+                    $scope.Methods.GetAllOrders($scope.currentPage, $scope.Data.SelectedDate);
                 },
-                GetAllOrders: function (pageNumber) {
+                GetAllOrders: function (pageNumber, selectedDate) {
                     var skip = (pageNumber - 1) * $scope.pageSize;
-                    httpService.get(apiConstant.getAllOrders + "?skip=" + skip, function (orders) {
+                    var filterDate = moment(selectedDate).format('L');
+
+                    httpService.get(apiConstant.orders + "?date=" + filterDate + "&&skip=" + skip + "&&limit=10", function (orders) {
                         $timeout(function () {
-                            $scope.Data.Orders = orders.Data.OrderList;
-                            $scope.Data.TotalOrders = orders.Data.TotalOrders;
+                            $scope.Data.Orders = orders.Data;
+                            $scope.Data.TotalOrders = orders.Data.length; //@todo: TotalOrders should be come from api not the length
                             $scope.Data.ShowingFrom = skip + 1;
-                            $scope.Data.ShowingTo = skip + orders.Data.OrderList.length;
+                            $scope.Data.ShowingTo = skip + orders.Data.length;
                         });
                     }, true);
                 },
                 PageChangeHandler: function (pageNum) {
                     $scope.currentPage = pageNum;
                     $scope.Methods.GetAllOrders(pageNum);
+                },
+                OnDateSelected: function() {
+                    $scope.Methods.GetAllOrders($scope.currentPage, $scope.Data.SelectedDate);
                 }
             };
 
